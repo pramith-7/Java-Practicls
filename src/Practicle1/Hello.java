bc54cb28657bb605d32f85c9c5dd712e86c752c5
pramith-7 <priyamanthaedirisooriya@gmail.com>
2023-09-28 08:50:36 +0530

    Practical 1

---
+++ src/Practicle1/Hello.java
@@ -0,0 +1,14 @@
+package Practicle1;
+public class Hello {
+
+    public static void main(String args[]){
+        int x = 10;
+        int y = 20;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+    }
+    
+}
---
+++ src/Practicle1/NameUni.java
@@ -0,0 +1,9 @@
+package Practicle1;
+public class NameUni {
+
+    public static void main(String[] args){
+        System.out.println("Name");
+        System.out.println("Uni");
+    }
+    
+}
